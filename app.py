a504b0646dcdfaa42576c20b32b1888aee7765f7
aya-aladdin <aya2aladdin@gmail.com>
2025-06-23 10:26:42 +0400

    done template, no words

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home.html') 
 def home():
     return render_template('home.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/sponsor.html')
+def sponsor():
+    return render_template('sponsor.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
